85a71b579630601dfc946734de04cb00f015e925
luxion <luxion0770@gmail.com>
2019-10-25 23:15:22 +0300

    add admin login controller with routes

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+require 'admin.php';
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::view('/admin', 'admin.dashboard.index');
+Route::view('/admin/login', 'admin.auth.login');
