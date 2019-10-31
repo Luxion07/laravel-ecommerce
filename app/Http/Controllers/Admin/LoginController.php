1288166e2a00fdda674697570e80b6df9835c04c
luxion <luxion0770@gmail.com>
2019-10-31 23:26:53 +0200

    add admin login / logout functionality

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -45,10 +45,9 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         $this->validate($request, [
-            'email' => 'required|email',
+            'email'   => 'required|email',
             'password' => 'required|min:6'
         ]);
-
         if (Auth::guard('admin')->attempt([
             'email' => $request->email,
             'password' => $request->password
