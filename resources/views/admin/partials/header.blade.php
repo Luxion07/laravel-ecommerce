853e4c6353f17ac8ea020836c4bb347ef79d78ee
Luxion07 <luxion0770@gmail.com>
2019-11-07 22:06:37 +0200

    Admin routes and SettingsController

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -3,13 +3,14 @@
     <a class="app-sidebar__toggle" href="#" data-toggle="sidebar" aria-label="Hide Sidebar"></a>
     <ul class="app-nav">
         <li class="app-search">
-            <input class="app-search__input" type="search" placeholder="Search" />
+            <input class="app-search__input" type="search" placeholder="Search"/>
             <button class="app-search__button">
                 <i class="fa fa-search"></i>
             </button>
         </li>
         <li class="dropdown">
-            <a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i class="fa fa-bell-o fa-lg"></i></a>
+            <a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Show notifications"><i
+                    class="fa fa-bell-o fa-lg"></i></a>
             <ul class="app-notification dropdown-menu dropdown-menu-right">
                 <li class="app-notification__title">
                     You have 4 new notifications.
@@ -55,16 +56,18 @@
         </li>
         <!-- User Menu-->
         <li class="dropdown">
-            <a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i class="fa fa-user fa-lg"></i></a>
+            <a class="app-nav__item" href="#" data-toggle="dropdown" aria-label="Open Profile Menu"><i
+                    class="fa fa-user fa-lg"></i></a>
             <ul class="dropdown-menu settings-menu dropdown-menu-right">
                 <li>
-                    <a class="dropdown-item" href="page-user.html"><i class="fa fa-cog fa-lg"></i> Settings</a>
+                    <a class="dropdown-item" href="{{ route('admin.settings') }}"><i class="fa fa-cog fa-lg"></i> Settings</a>
                 </li>
                 <li>
                     <a class="dropdown-item" href="page-user.html"><i class="fa fa-user fa-lg"></i> Profile</a>
                 </li>
                 <li>
-                    <a class="dropdown-item" href="{{ route('admin.logout') }}"><i class="fa fa-sign-out fa-lg"></i> Logout</a>
+                    <a class="dropdown-item" href="{{ route('admin.logout') }}"><i class="fa fa-sign-out fa-lg"></i>
+                        Logout</a>
                 </li>
             </ul>
         </li>
--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,6 @@
 Route::group(['prefix' => 'admin'], function () {
 
     Route::get('login', 'Admin\LoginController@showLoginForm')->name('admin.login');
-//    Route::get('log-in', 'Admin\LoginController@showLoginForm')->name('admin.login');
     Route::post('login', 'Admin\LoginController@login')->name('admin.login.post');
     Route::get('logout', 'Admin\LoginController@logout')->name('admin.logout');
 
@@ -12,6 +11,9 @@ Route::group(['prefix' => 'admin'], function () {
         Route::get('/', function () {
             return view('admin.dashboard.index');
         })->name('admin.dashboard');
+
+        Route::get('/settings', 'Admin\SettingController@index')->name('admin.settings');
+        Route::post('/settings', 'Admin\SettingController@update')->name('admin.settings.update');
 
     });
 
